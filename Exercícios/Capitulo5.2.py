31f0ff5771ff96f8067dc3d0bf3ad560a5800d22
Gabriel Reis <lugafeagre@gmail.com>
2023-03-01 23:27:18 -0300

    fix: uncomment the scripts

--- Exercícios/Capitulo5.2.py
+++ Exercícios/Capitulo5.2.py
@@ -1,141 +1,141 @@
 #Exercício 5.11
-# depósito = float(input('Digite o valor do depósito: '))
-# taxa = float(input('Digite o valor da taxa de juros anual: '))
-# n = 1
-# valor_acumulado = depósito
-# while n <= 24:
-#     valor_acumulado = valor_acumulado + valor_acumulado * ((taxa/100)/12)
-#     print(f'{valor_acumulado:6.2f}')
-#     n = n + 1
-# print(f'O valor final é: {valor_acumulado:6.2f}')
+depósito = float(input('Digite o valor do depósito: '))
+taxa = float(input('Digite o valor da taxa de juros anual: '))
+n = 1
+valor_acumulado = depósito
+while n <= 24:
+    valor_acumulado = valor_acumulado + valor_acumulado * ((taxa/100)/12)
+    print(f'{valor_acumulado:6.2f}')
+    n = n + 1
+print(f'O valor final é: {valor_acumulado:6.2f}')
 
 #Exercício 5.12
-# depósito_inicial = float(input('Digite o valor do depósito: '))
-# taxa = float(input('Digite o valor da taxa de juros anual: '))
-# depósito_mensal = float(input('Digite o valor do aporte mensal: '))
-# n = 1
-# valor_acumulado = depósito_inicial
-# while n <= 24:
-#     valor_acumulado = valor_acumulado + depósito_mensal  + valor_acumulado * ((taxa/100)/12)
-#     print(f'{valor_acumulado:6.2f}')
-#     n = n + 1
-# print(f'O valor final é: {valor_acumulado:6.2f}')
+depósito_inicial = float(input('Digite o valor do depósito: '))
+taxa = float(input('Digite o valor da taxa de juros anual: '))
+depósito_mensal = float(input('Digite o valor do aporte mensal: '))
+n = 1
+valor_acumulado = depósito_inicial
+while n <= 24:
+    valor_acumulado = valor_acumulado + depósito_mensal  + valor_acumulado * ((taxa/100)/12)
+    print(f'{valor_acumulado:6.2f}')
+    n = n + 1
+print(f'O valor final é: {valor_acumulado:6.2f}')
 
 #Exercício 5.13
-# dívida = float(input('Digite o valor da dívida: '))
-# taxa = float(input('Digite o valor da taxa de juros mensal: '))
-# depósito_mensal = float(input('Digite o valor do aporte mensal: '))
-# valor_acumulado = dívida
-# número_meses = 0
-# total_pago = 0
-# total_juros = 0
-# while valor_acumulado > 0:
-#     número_meses = número_meses + 1
-#     valor_acumulado = valor_acumulado - depósito_mensal  + valor_acumulado * (taxa/100)
-#     total_pago = total_pago + depósito_mensal
-#     if valor_acumulado < 0:
-#         total_pago = total_pago + valor_acumulado
-# total_juros = total_pago - dívida
-# print(f'O número de meses é {número_meses}')
-# print(f'O valor pago é {total_pago:.2f} e o valor dos respectivos juros é {total_juros:.2f}')
+dívida = float(input('Digite o valor da dívida: '))
+taxa = float(input('Digite o valor da taxa de juros mensal: '))
+depósito_mensal = float(input('Digite o valor do aporte mensal: '))
+valor_acumulado = dívida
+número_meses = 0
+total_pago = 0
+total_juros = 0
+while valor_acumulado > 0:
+    número_meses = número_meses + 1
+    valor_acumulado = valor_acumulado - depósito_mensal  + valor_acumulado * (taxa/100)
+    total_pago = total_pago + depósito_mensal
+    if valor_acumulado < 0:
+        total_pago = total_pago + valor_acumulado
+total_juros = total_pago - dívida
+print(f'O número de meses é {número_meses}')
+print(f'O valor pago é {total_pago:.2f} e o valor dos respectivos juros é {total_juros:.2f}')
 
 #Exercício 5.14
-# soma = 0
-# números_somados = 0
-# while True:
-# 	variable = int(input('digite um número a somar ou 0 para sair: '))
-# 	soma += variable
-# 	números_somados += 1
-# 	média = soma / números_somados
-# 	if variable == 0:
-# 		break
-# print(f'soma: {soma}')
-# print(f'números somados: {números_somados}')
-# print(f'média: {média}')
+soma = 0
+números_somados = 0
+while True:
+	variable = int(input('digite um número a somar ou 0 para sair: '))
+	soma += variable
+	números_somados += 1
+	média = soma / números_somados
+	if variable == 0:
+		break
+print(f'soma: {soma}')
+print(f'números somados: {números_somados}')
+print(f'média: {média}')
 
 #Exercício 5.15
-# soma = 0
-# while True:
-# 	cod = int(input('digite o código do produto ou 0 para encerrar: '))
-# 	if cod == 0:
-# 		break
-# 	elif cod == 1:
-# 		qnt = int(input('digite a quantidade: '))
-# 		soma = soma + qnt * 0.5
-# 	elif cod == 2:
-# 		qnt = int(input('digite a quantidade: '))
-# 		soma = soma + qnt * 1
-# 	elif cod == 3:
-# 		qnt = int(input('digite a quantidade: '))
-# 		soma = soma + qnt * 4
-# 	elif cod == 5:
-# 		qnt = int(input('digite a quantidade: '))
-# 		soma = soma + qnt * 7
-# 	elif cod == 9:
-# 		qnt = int(input('digite a quantidade: '))
-# 		soma = soma + qnt * 8
-# 	else:
-# 		print('Código Inválido')
-# print(f'O total de compras é R$ {soma}')
+soma = 0
+while True:
+	cod = int(input('digite o código do produto ou 0 para encerrar: '))
+	if cod == 0:
+		break
+	elif cod == 1:
+		qnt = int(input('digite a quantidade: '))
+		soma = soma + qnt * 0.5
+	elif cod == 2:
+		qnt = int(input('digite a quantidade: '))
+		soma = soma + qnt * 1
+	elif cod == 3:
+		qnt = int(input('digite a quantidade: '))
+		soma = soma + qnt * 4
+	elif cod == 5:
+		qnt = int(input('digite a quantidade: '))
+		soma = soma + qnt * 7
+	elif cod == 9:
+		qnt = int(input('digite a quantidade: '))
+		soma = soma + qnt * 8
+	else:
+		print('Código Inválido')
+print(f'O total de compras é R$ {soma}')
 
 # Exercício 5.18
-# valor = float(input('Digite o valor a pagar: '))
-# cédulas = 0
-# atual = 100
-# a_pagar = valor
-# while True:
-# 	if atual <= a_pagar:
-# 		a_pagar -= atual
-# 		cédulas += 1
-# 	else:
-# 		print(f'{cédulas} cédula(s) de R$ {atual}')
-# 		if a_pagar == 0:
-# 			break
-# 		if atual == 100:
-# 			atual = 50
-# 		elif atual == 50:
-# 			atual = 20
-# 		elif atual == 20:
-# 			atual = 10
-# 		elif atual == 10:
-# 			atual = 5
-# 		elif atual == 5:
-# 			atual = 1
-# 		elif atual == 1:
-# 			break
-# 		cédulas = 0
+valor = float(input('Digite o valor a pagar: '))
+cédulas = 0
+atual = 100
+a_pagar = valor
+while True:
+	if atual <= a_pagar:
+		a_pagar -= atual
+		cédulas += 1
+	else:
+		print(f'{cédulas} cédula(s) de R$ {atual}')
+		if a_pagar == 0:
+			break
+		if atual == 100:
+			atual = 50
+		elif atual == 50:
+			atual = 20
+		elif atual == 20:
+			atual = 10
+		elif atual == 10:
+			atual = 5
+		elif atual == 5:
+			atual = 1
+		elif atual == 1:
+			break
+		cédulas = 0
 
 # Exercício 5.19
-# valor = float(input('Digite o valor a pagar: '))
-# cédulas = 0
-# atual = 100
-# a_pagar = valor
-# while True:
-# 	if atual <= a_pagar:
-# 		a_pagar -= atual
-# 		cédulas += 1
-# 	else:
-# 		print(f'{cédulas} cédula(s) de R$ {atual}')
-# 		if a_pagar == 0:
-# 			break
-# 		if atual == 100:
-# 			atual = 50
-# 		elif atual == 50:
-# 			atual = 20
-# 		elif atual == 20:
-# 			atual = 10
-# 		elif atual == 10:
-# 			atual = 5
-# 		elif atual == 5:
-# 			atual = 1
-# 		elif atual == 1:
-# 			atual = 0.5
-# 		elif atual == 0.5:
-# 			atual = 0.25
-# 		elif atual == 0.25:
-# 			atual = 0.10	
-# 		elif atual == 0.10:
-# 			atual = 0.05
-# 		elif atual == 0.05:
-# 			break
-# 		cédulas = 0
+valor = float(input('Digite o valor a pagar: '))
+cédulas = 0
+atual = 100
+a_pagar = valor
+while True:
+	if atual <= a_pagar:
+		a_pagar -= atual
+		cédulas += 1
+	else:
+		print(f'{cédulas} cédula(s) de R$ {atual}')
+		if a_pagar == 0:
+			break
+		if atual == 100:
+			atual = 50
+		elif atual == 50:
+			atual = 20
+		elif atual == 20:
+			atual = 10
+		elif atual == 10:
+			atual = 5
+		elif atual == 5:
+			atual = 1
+		elif atual == 1:
+			atual = 0.5
+		elif atual == 0.5:
+			atual = 0.25
+		elif atual == 0.25:
+			atual = 0.10	
+		elif atual == 0.10:
+			atual = 0.05
+		elif atual == 0.05:
+			break
+		cédulas = 0
--- Exercícios/Capitulo6.1.py
+++ Exercícios/Capitulo6.1.py
@@ -1,97 +1,97 @@
 # Exercício 6.1
-# notas = [0, 0, 0, 0, 0, 0, 0]
-# soma = 0
-# x = 0
-# while x < 7:
-#     notas[x] = float(input(f'Nota {x + 1}: '))
-#     soma += notas [x]
-#     x += 1
-# x = 0
-# while x < 7:
-#     print(f'Nota {x + 1}: {notas [x]:6.2f}')
-#     x += 1
-# print(f'Média: {soma / x:5.2f}')
+notas = [0, 0, 0, 0, 0, 0, 0]
+soma = 0
+x = 0
+while x < 7:
+    notas[x] = float(input(f'Nota {x + 1}: '))
+    soma += notas [x]
+    x += 1
+x = 0
+while x < 7:
+    print(f'Nota {x + 1}: {notas [x]:6.2f}')
+    x += 1
+print(f'Média: {soma / x:5.2f}')
 
 # Exercício 6.2
-# listA = []
-# listB = []
-# listC = []
-# x = 0
-# while True:
-#     x = str(input('Insira o valor a ser adicionado na primeira lista, digite "s" para sair: '))
-#     if x == "s":
-#         break
-#     else:
-#         listA.append(x)
-# x = 0
-# while True:
-#     x = str(input('Insira o valor a ser adicionado na segunda lista, digite "s" para sair: '))
-#     if x == "s":
-#         break
-#     else:
-#         listB.append(x)
-# listC.extend (listA + listB)
-# if listC == []:
-#     print('Não há valores informados')
-# else:
-#     print(f'Os valores unificados das duas listas são: {listC}')
+listA = []
+listB = []
+listC = []
+x = 0
+while True:
+    x = str(input('Insira o valor a ser adicionado na primeira lista, digite "s" para sair: '))
+    if x == "s":
+        break
+    else:
+        listA.append(x)
+x = 0
+while True:
+    x = str(input('Insira o valor a ser adicionado na segunda lista, digite "s" para sair: '))
+    if x == "s":
+        break
+    else:
+        listB.append(x)
+listC.extend (listA + listB)
+if listC == []:
+    print('Não há valores informados')
+else:
+    print(f'Os valores unificados das duas listas são: {listC}')
 
 # Exercício 6.3
-# listA = []
-# listB = []
-# listC = []
-# x = 0
-# while True:
-#     x = str(input('Insira o valor a ser adicionado na primeira lista, digite "s" para sair: '))
-#     if x == "s":
-#         break
-#     else:
-#         listA.append(x)
-# x = 0
-# while True:
-#     x = str(input('Insira o valor a ser adicionado na segunda lista, digite "s" para sair: '))
-#     if x == "s":
-#         break
-#     else:
-#         listB.append(x)
-# n = 0
-# while n < len(listA):
-#     if listA[n] not in listC:
-#         listC.extend (listA[n])
-#     n += 1
-# n = 0
-# while n < len(listB):
-#     if listB[n] not in listC:
-#         listC.extend (listB[n])
-#     n += 1
-# if listC == []:
-#     print('Não há valores informados')
-# else:
-#     print(f'Os valores unificados das duas listas são: {listC}')
+listA = []
+listB = []
+listC = []
+x = 0
+while True:
+    x = str(input('Insira o valor a ser adicionado na primeira lista, digite "s" para sair: '))
+    if x == "s":
+        break
+    else:
+        listA.append(x)
+x = 0
+while True:
+    x = str(input('Insira o valor a ser adicionado na segunda lista, digite "s" para sair: '))
+    if x == "s":
+        break
+    else:
+        listB.append(x)
+n = 0
+while n < len(listA):
+    if listA[n] not in listC:
+        listC.extend (listA[n])
+    n += 1
+n = 0
+while n < len(listB):
+    if listB[n] not in listC:
+        listC.extend (listB[n])
+    n += 1
+if listC == []:
+    print('Não há valores informados')
+else:
+    print(f'Os valores unificados das duas listas são: {listC}')
 
 # Exercício 6.5
-# último = 10
-# fila = list(range(1, último + 1))
-# while True:
-#     print(f'\nExistem {len(fila)} clientes na fila')
-#     print(f'Fila atual: {fila}')
-#     print('Digite F para adicionar um cliente ao fim da fila\nou A para realizar o atendimento.\n')
-#     sair = str(input('Pressione S para sair ou ENTER para continuar: '))
-#     if sair.lower() == 's':
-#         break    
-#     operação = str(input('Operação (F ou A): '))
-#     listA = list(operação)
-#     while len(listA) > 0:
-#         fatia_string = listA[0]
-#         del listA[0]
-#         if fatia_string.lower() == 'a':
-#             if len(fila) > 0:
-#                 atendido = fila.pop(0)
-#                 print(f'Cliente {atendido} atendido')
-#             else:
-#                 print('Fila vazia! Ninguém para atender.')
-#         elif fatia_string.lower() == 'f':
-#             último += 1
-#             fila.append(último)
-#         else:
-#             print('Operação inválida! Digite apenas F ou A!')
+último = 10
+fila = list(range(1, último + 1))
+while True:
+    print(f'\nExistem {len(fila)} clientes na fila')
+    print(f'Fila atual: {fila}')
+    print('Digite F para adicionar um cliente ao fim da fila\nou A para realizar o atendimento.\n')
+    sair = str(input('Pressione S para sair ou ENTER para continuar: '))
+    if sair.lower() == 's':
+        break    
+    operação = str(input('Operação (F ou A): '))
+    listA = list(operação)
+    while len(listA) > 0:
+        fatia_string = listA[0]
+        del listA[0]
+        if fatia_string.lower() == 'a':
+            if len(fila) > 0:
+                atendido = fila.pop(0)
+                print(f'Cliente {atendido} atendido')
+            else:
+                print('Fila vazia! Ninguém para atender.')
+        elif fatia_string.lower() == 'f':
+            último += 1
+            fila.append(último)
+        else:
+            print('Operação inválida! Digite apenas F ou A!')
