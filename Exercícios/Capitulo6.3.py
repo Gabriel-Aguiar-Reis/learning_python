e824cb50dadd87c7d6779934ce6c2aea3eb3d35e
Gabriel Reis <lugafeagre@gmail.com>
2023-03-09 19:58:32 -0300

    Bump: making more exercises>

--- Exercícios/Capitulo6.3.py
+++ Exercícios/Capitulo6.3.py
@@ -77,6 +77,33 @@ for e in L:
     print(e)
     
 # Exercício 6.17
-# Exercício 6.18
-# Exercício 6.19
-# Exercício 6.20
+estoque = {'tomate': [1000, 2.30],
+           'alface': [500, 0.45],
+           'batata': [200, 1.20],
+           'feijão': [100, 1.50]}
+venda = []
+while True:
+    entrada = str(input('Digite o nome do produto a adicionar\ne "fim" para continuar: '))
+    for e in estoque:
+        if entrada.lower() == e:
+            entrada_quantidade = int(input('Digite a quantidade: '))
+            venda.append([entrada, entrada_quantidade])
+    if entrada not in estoque.keys() and entrada != 'fim':
+        print('Produto não existe!')
+    if entrada.lower() == 'fim':
+        break
+total = 0
+print('Vendas:\n')
+for operação in venda:
+    produto, quantidade = operação
+    preço = estoque[produto][1]
+    custo = preço * quantidade
+    print(f'{produto:12s}: {quantidade:3d} x {preço:6.2f} = {custo:6.2f}')
+    estoque[produto][0] -= quantidade
+    total += custo
+print(f' Custo total: {total:21.2f}\n')
+print('Estoque:\n')
+for chave, dados in estoque.items():
+    print('Descrição: ', chave)
+    print('Quantidade: ', dados[0])
+    print(f'Preço: {dados[1]:6.2f}')
---
+++ Exercícios/Capitulo6.4.py
@@ -0,0 +1,31 @@
+# Exercício 6.18
+entrada = str(input('Digite a frase que desejar: '))
+dicionario = {}
+for e in entrada:
+    if e in dicionario:
+        dicionario[e] += 1
+    else:
+        dicionario[e] = 1
+print(dicionario)
+
+# Exercício 6.19
+a = set([1, 2, 3, 4, 5])
+b = set([3, 4, 5, 6, 7])
+c = a - b
+d = b - a
+e = a | b
+print(f"""\nValores comuns às duas listas: {e - c - d}
+Valores que só existem na primeira lista: {c}
+Valores que só existem na segunda lista: {d}
+Valores não repetidos: {c | d}
+Primeira lista sem elementos repetidos da segunda: {e}""")
+
+# Exercício 6.20
+lista1 = set([1, 2, 3, 4, 5])
+lista2 = set([1, 9, 3, 7, 5, 6, 4])
+m = lista1 - lista2
+n = lista2 - lista1
+o = lista1 | lista2
+print(f"""\nElementos que não mudaram: {o - m - n}
+Novos elementos: {n}
+Elementos que foram removidos: {m}""")
